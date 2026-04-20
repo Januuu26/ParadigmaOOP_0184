49a0e6862c15268e417ee4d96e891534be52ec67
Januuu26 <b77286116@gmail.com>
2026-04-20 14:35:01 +0700

    memebuat

--- intruductionOOP.cpp
+++ intruductionOOP.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+    float nilai;
 
+    void printData() {
+        cout << "NIM: " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+    }
+};
+
+int main() {
+    Mahasiswa mhs;
+    mhs.nim = 12345;
+    mhs.nama = "Abi";
+    mhs.nilai = 85.5;
+
+    mhs.printData();
+}
